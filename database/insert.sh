5e04b456a917146151c118944f59907525e9e039
ramuller_fi <ralf.mueller@live.com>
2025-06-09 08:44:51 +0300

    add database related files to repo

---
+++ database/insert.sh
@@ -0,0 +1,1 @@
+mysql -h leo -u pokemat -ppokemat123 -e INSERT INTO trainers '(name,trainer_code,team) VALUES ("Higimmi1234","7692 0024 9612","yellow");' pokemat
---
+++ database/update.sh
@@ -0,0 +1,1 @@
+mysql -h leo -u pokemat -ppokemat123 -e UPDATE trainers SET name="Higimmi1234"); pokemat
