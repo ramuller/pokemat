ad8dd7dec763cba2fcc60fdf1d380a21221414ca
ramuller_fi <6991579+ramuller@users.noreply.github.com>
2026-01-15 21:57:35 +0200

    added net-mon and fix

---
+++ bin/net-mon.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# port=$1
+
+source $SCRIPT_DIR/phone-lib.sh
+
+disable_air()
+{
+    serial=$1
+    echo "Disable airplane mode on $serial"
+    adb -s $serial shell su -c 'settings put global airplane_mode_on 0' >/dev/null 2&>1
+    adb -s $serial shell su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false' 
+    adb -s $serial shell su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true'
+}
+
+enable_air()
+{
+    serial=$1
+    echo "Enable airplane mode on $serial"
+    adb -s $serial shell su -c 'settings put global airplane_mode_on 1' >/dev/null 2&>1
+    adb -s $serial shell su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true'
+    adb -s $serial shell su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false' 
+}
+
+while true
+do
+    for s in $(adb devices |grep device$  |cut -f 1) ; do
+        echo device $s
+        adb -s $s shell dumpsys connectivity |grep --silent wlan0:
+        if [ $? -ne 0 ]; then
+            echo NetDown
+            enable_air $s
+            sleep 2 
+            disable_air $s
+            
+        fi
+    done
+    sleep 20
+done
+
+    
