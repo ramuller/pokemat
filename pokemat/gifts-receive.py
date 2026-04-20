b6ebf12321c34bf7cba0f17d555f61a4c44873b1
ramuller_fi <6991579+ramuller@users.noreply.github.com>
2026-04-20 20:43:14 +0300

    gifts

--- pokemat/gifts-receive.py
+++ pokemat/gifts-receive.py
@@ -75,9 +75,9 @@ def gifting(port):
             if phone.buttons.i_x_clear_text.press(retries=1):
                sleep(0.5)
 
-            # bt = phone.buttons.i_friends_search.search(retries=1)
+            bt = phone.buttons.i_friends_search.search(retries=1)
             # sleep(0.5)
-            if not phone.buttons.i_friends_search.press():
+            if not phone.buttons.t_friends_search.press():
                 print('No SEARCH button found')
                 # phone.screen.go_home()
                 # raise Exception('No SEARCH button found')
@@ -117,7 +117,7 @@ def gifting(port):
 
             # Back to friends
             max_tries = 0
-            b = phone.buttons.i_friends_search.search(retries=1)
+            b = phone.buttons.t_friends.search(retries=1)
             while not b:
                 phone.buttons.i_exits.press(retries=1)
                 sleep(2)
