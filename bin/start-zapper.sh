123453521c26197f30e672395f1b72e27fe23e43
ramuller_fi <6991579+ramuller@users.noreply.github.com>
2026-04-25 20:01:55 +0300

    tuning

--- bin/start-zapper.sh
+++ bin/start-zapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ZAPPER_DIR=$HOME/git/scrcpyzapper
+ZAPPER_DIR=$HOME/git/merge/scrcpyzapper
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/phone-lib.sh
@@ -20,13 +21,12 @@ source $SCRIPT_DIR/phone-lib.sh
 # Aphex
 # export SN=R58N648E8EX
 
-# MAX_SPEED=396500
-MAX_SPEED=1040000
 
 # [ -n "$EXTRA" ] || EXTRA="-S --disable-screensaver"
 [ -n "$EXTRA" ] || EXTRA="--disable-screensaver"
 
-STD_ARGS="-m 1024 --max-fps=8 --no-audio-playback --raw-key-events --no-resize"
+STD_ARGS="-m 1024 --max-fps=8 --no-audio-playback --raw-key-events --no-resize --audio-codec=aac"
+STD_ARGS="-m 1024 --max-fps=8 --no-audio-playback --raw-key-events --audio-codec=aac"
 
 cd $ZAPPER_DIR
 
@@ -37,40 +37,46 @@ elif [ "$1" == "1" ];then
     echo SN=$SN
     [ -z "$SN" ] && SN=ce091609f2237a1904
     echo SN=$SN
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=00 --window-y=0 --rest-api-port=300$1 
 elif [ "$1" == "2" ];then
     # [ -z $SN ] && SN=ce091719d290250304
     [ -z $SN ] && SN=$SN_3002
     echo SN=$SN
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=600 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "3" ];then
     [ -z $SN ] && SN=ce12160c4dac273705
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=1200 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "4" ];then
     [ -z $SN ] && SN=ce0117119076d82a0c
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=1800 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "5" ];then
     [ -z $SN ] && SN=ce091609ec79190d04
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=2400 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "6" ];then
     [ -z $SN ] && SN=ce11160bc1ce7d1705
-    adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
+    adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=3000 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "7" ];then
     # [ -z $SN ] && SN=ce01182118d5b02a0c
     [ -z $SN ] && SN=$SN_3007
-    # adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
-    ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=3000 --window-y=1030 --rest-api-port=300$1
+    # adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
+    ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=100 --window-y=0 --rest-api-port=300$1
 elif [ "$1" == "8" ];then
     # [ -z $SN ] && SN=ce01182118d5b02a0c
     [ -z $SN ] && SN=$SN_3008
-    # adb -s $SN shell "su -c \"echo $MAX_SPEED >/sys/power/cpufreq_max_limit\""
-    ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=0 --window-y=1030 --rest-api-port=300$1
+    # adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
+    ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=80 --window-y=1030 --rest-api-port=300$1
+elif [ "$1" == "9" ];then
+    # [ -z $SN ] && SN=ce01182118d5b02a0c
+    [ -z $SN ] && SN=$SN_3009
+    # adb -s $SN shell "su -c \"echo 1040000 >/sys/power/cpufreq_max_limit\""
+    ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=100 --window-y=1030 --rest-api-port=300$1
+    # ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=100 --window-y=1030 --rest-api-port=300$1 -m 800
 elif [ "$1" == "a40" ];then
     [ -z $SN ] && SN=R58M93ND7BF
     ./run x $EXTRA --window-title="$1" -s $SN $STD_ARGS --window-x=0 --window-y=1070 --rest-api-port=3040 
@@ -79,5 +85,5 @@ elif [ "$1" == "u" ];then
     ./run x $EXTRA --window-title=3010 $STD_ARGS --window-x=0 --window-y=0 --rest-api-port=3010
 else
     echo unknow phone using port 3099 title $SN
-    ./run x $EXTRA --window-title="$SN" -s $SN $STD_ARGS --rest-api-port=3099
+    ./run x $EXTRA --window-title="3099-SN" -s $SN $STD_ARGS --disable-screensaver --rest-api-port=3099
 fi
