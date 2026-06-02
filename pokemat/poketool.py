950f442fe33770c9b1c65305b6abfff5c56f535f
ramuller_fi <6991579+ramuller@users.noreply.github.com>
2026-06-02 22:19:16 +0300

    add kill scrcpy server script

--- pokemat/poketool.py
+++ pokemat/poketool.py
@@ -258,13 +258,17 @@ def my_test():
     # return True
     # phone.egg_handle(force=False)
     # phone.screen.go_home()
+
+
     print('My test')
     count=1
     while True:
-        print('Rotage {}'.format(count))
+        b = phone.buttons.i_catch_ball.search(retries=1, verbose=0)
+
+        print(f'Rotage {count} : {b}')
         count+=1
-        phone.rotate()
-        sleep(0.5)
+        #phone.rotate()
+        # sleep(0.5)
 
 
 
